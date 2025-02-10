13b0b4d0353ccc841bbd83f0b410452dcad31635
Yurii Mezentsev <yvmezentsev@gmail.com>
2025-02-10 22:29:55 +0200

    init commit

---
+++ src/main/java/com/ua/teamchallenge/handmade_store/HandmadeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.ua.teamchallenge.handmade_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandmadeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandmadeStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ua/teamchallenge/handmade_store/HandmadeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.teamchallenge.handmade_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandmadeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
