df0b3c714b1952639fe9839e37934eb80f221fe3
Yurii Mezentsev <yvmezentsev@gmail.com>
2025-02-13 10:16:56 +0200

    update UserRepository and add TestController for check jwtToken

--- src/main/java/com/ua/teamchallenge/handmadestore/repository/UserRepository.java
+++ src/main/java/com/ua/teamchallenge/handmadestore/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.ua.teamchallenge.handmadestore.repository;
 
 import com.ua.teamchallenge.handmadestore.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"roles"})
     Optional<User> findByUsernameIgnoreCase(String username);
 }
